0aa57ee8d1783c813a467f8a54657994d84b4523
Daniel Chagas <daniel.boasq@gmail.com>
2025-03-31 10:12:40 -0300

    feat: sale validator

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/SaleValidator.cs
@@ -0,0 +1,37 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation;
+
+internal class SaleValidator : AbstractValidator<Sale>
+{
+    public SaleValidator()
+    {
+        RuleFor(sale => sale.SaleNumber)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .Length(1, 50).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.");
+
+        RuleFor(sale => sale.Date)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .LessThanOrEqualTo(DateTime.Now).WithMessage("{PropertyName} cannot be in the future.");
+
+        RuleFor(sale => sale.CustomerId)
+            .NotEmpty().WithMessage("{PropertyName} is required.");
+
+        RuleFor(sale => sale.CustomerName)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .Length(1, 100).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.");
+
+        RuleFor(sale => sale.BranchId)
+            .NotEmpty().WithMessage("{PropertyName} is required.");
+
+        RuleFor(sale => sale.BranchName)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .Length(1, 100).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.");
+
+        RuleFor(sale => sale.Items)
+            .NotEmpty().WithMessage("Sale must have at least one item.")
+            .Must(items => items.All(item => item.Quantity > 0)).WithMessage("All sale items must have a quantity greater than zero.")
+            .Must(items => items.All(item => item.UnitPrice > 0)).WithMessage("All sale items must have a unit price greater than zero.");
+    }
+}
