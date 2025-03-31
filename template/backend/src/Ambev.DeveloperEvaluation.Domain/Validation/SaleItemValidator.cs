e122a82cae32861fe5b149e8d517abc4e882c6f2
Daniel Chagas <daniel.boasq@gmail.com>
2025-03-31 10:15:52 -0300

    feat: sale item validator

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/SaleItemValidator.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation;
+
+internal class SaleItemValidator : AbstractValidator<SaleItem>
+{
+    public SaleItemValidator()
+    {
+        RuleFor(saleItem => saleItem.ProductId)
+            .NotEmpty().WithMessage("{PropertyName} cannot be empty.")
+            .Length(1, 50).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.");
+
+        RuleFor(saleItem => saleItem.ProductName)
+            .NotEmpty().WithMessage("{PropertyName} cannot be empty.")
+            .Length(1, 100).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.");
+
+        RuleFor(saleItem => saleItem.Quantity)
+            .GreaterThan(0).WithMessage("{PropertyName} must be greater than {ComparisonValue}.");
+
+        RuleFor(saleItem => saleItem.UnitPrice)
+            .GreaterThan(0).WithMessage("{PropertyName} must be greater than {ComparisonValue}.");
+
+        RuleFor(saleItem => saleItem.Discount)
+            .GreaterThanOrEqualTo(0).WithMessage("{PropertyName} cannot be negative.")
+            .LessThanOrEqualTo(saleItem => saleItem.UnitPrice * saleItem.Quantity).WithMessage("{PropertyName} cannot be greater than the total price.");
+    }
+}
